445fab7ade07be18f0e722d1ce8205bb8f2838ca
Denis <d.rotari17@gmail.com>
2022-12-04 23:45:59 +0200

    remove material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { HttpClientModule } from '@angular/common/http'
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
-import { MatDialogModule, MatDialogRef } from '@angular/material/dialog'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
@@ -10,13 +9,8 @@ import { ProductPageComponent } from './product-page/product-page.component'
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, ProductPageComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, MatDialogModule],
-  providers: [
-    {
-      provide: MatDialogRef,
-      useValue: {}
-    }
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
